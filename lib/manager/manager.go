398799302aec5ba87318eb96d1514840a3dc6355
GenPage <genpage@pagefortress.com>
2017-06-09 16:57:50 -0400

    Implement manager logic

--- lib/manager/manager.go
+++ lib/manager/manager.go
@@ -1,9 +1,17 @@
 package manager
 
 import (
+	"errors"
 	"sync"
 
 	"github.com/genpage/do-backend-challenge/lib/model"
+)
+
+var (
+	errAlreadyExists    = errors.New("ERROR\n")
+	errDoesNotExist     = errors.New("ERROR\n")
+	errMissingDepedency = errors.New("ERROR\n")
+	errDepedencyFound   = errors.New("ERROR\n")
 )
 
 type Manager interface {
@@ -23,14 +31,56 @@ func NewManager() Manager {
 	}
 }
 
-func (r *manager) Index(p *model.Package) error {
+func (m *manager) Index(p *model.Package) error {
+	m.packagesMx.Lock()
+	defer m.packagesMx.Unlock()
+
+	//Check if package already in map
+	err := m.Query(p)
+	if err == nil {
+		return errAlreadyExists
+	}
+
+	//Make sure package has depedencies in map
+	for _, dep := range p.Depedencies {
+		if _, ok := m.packages[dep]; !ok {
+			return errMissingDepedency
+		}
+	}
+
+	//Insert package into map
+	m.packages[p.Name] = p
 	return nil
 }
 
-func (r *manager) Remove(p *model.Package) error {
+func (m *manager) Remove(p *model.Package) error {
+	m.packagesMx.Lock()
+	defer m.packagesMx.Unlock()
+
+	//Check if package already in map
+	err := m.Query(p)
+	if err != nil {
+		return nil
+	}
+
+	//Check for any remaining dependencies
+	for _, dep := range p.Depedencies {
+		if _, ok := m.packages[dep]; ok {
+			return errDepedencyFound
+		}
+	}
+
+	delete(m.packages, p.Name)
 	return nil
 }
 
-func (r *manager) Query(p *model.Package) error {
+func (m *manager) Query(p *model.Package) error {
+	m.packagesMx.Lock()
+	defer m.packagesMx.Unlock()
+
+	if _, ok := m.packages[p.Name]; !ok {
+		return errDoesNotExist
+	}
+
 	return nil
 }
